71140cb41d32ebf27072a003928b9cf57efef554
Karsten Kaj Jakobsen <kj@patientsky.com>
2019-02-25 13:21:02 +0100

    Added download of images on first run

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@ set -e
 
 envsubst < /default.conf.template > /etc/nginx/conf.d/default.conf
 
+echo "Downloading BTC,BLOCK snapshots..."
+wget -O /var/www/static/utxo-snapshot-bitcoin.tar http://utxosets.blob.core.windows.net/public/utxo-snapshot-bitcoin-mainnet-551636.tar
+wget -O /var/www/static/blocknetdex.zip https://github.com/BlocknetDX/blocknet-blockchain-bootstrap/releases/download/v1.0/BlocknetDX.zip
+
 exec nginx -g 'daemon off;'
